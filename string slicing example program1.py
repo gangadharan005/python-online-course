1288c5b4bf673d7a3e533dfb39a827aaba65187c
gangadharan005 <100509585+gangadharan005@users.noreply.github.com>
2022-03-14 12:22:36 +0530

    Add files via upload

---
+++ string slicing example program1.py
@@ -0,0 +1,80 @@
+# Example-1
+x = "INFORMATION_TECHNOLOGY"
+print(x[-2:0:-1])
+print(x[5:-5:5])
+print(x[-5:5:-5])
+print(x[3:9:-1])
+print(x[9:-10:-2])
+print(x[2:-4:-4])
+print(x[-4:2:-2])
+print(x[0:-1:-3])
+print(x[-3:3:6])
+print(x[::-3])
+print(x[::-2])
+print(x[2:-2:-2])
+print(x[2:-2:2])
+print(x[5:-5:5])
+print(x[3:-3:3])
+print(x[::2])
+print(x[::-5])
+
+
+#Example - 2
+y = "PYTHON_USING_INTERPRETER"
+print(y[-2:0:-1])
+print(y[5:-5:5])
+print(y[-5:5:-5])
+print(y[3:9:-1])
+print(y[9:-10:-2])
+print(y[2:-4:-4])
+print(y[-4:2:-2])
+print(y[0:-1:-3])
+print(y[-3:3:6])
+print(y[::-3])
+print(y[::-2])
+print(y[2:-2:-2])
+print(y[2:-2:2])
+print(y[3:-3:3])
+print(y[::2])
+print(y[::-5])
+
+# Example-3
+a = "COMPUTER SCIENCE"
+print(a[-10:10:3])
+print(a[-5:-1:1])
+print(a[-8:-2:10])
+print(a[5::3])
+print(a[:5:2])
+print(a[10:14:2])
+print(a[-10:-3:3])
+print(a[5:12:5])
+print(a[3:-3:3])
+print(a[-5:-3:20])
+
+# Example-4
+b = "INFORMATION_TECHNOLOGY"
+print(b[-10:10:3])
+print(b[-5:-1:-1])
+print(b[5::3])
+print(b[:5:2])
+print(b[10:14:2])
+print(b[-10:-3:3])
+print(b[5:12:5])
+print(b[3:-3:3])
+print(b[-5:-3:20])
+
+#Example - 5
+c = "GOOGLE_CHROME_BROWSER"
+print(c[-10:10:3])
+print(c[-5:-1:1])
+print(c[-8:-2:10])
+print(c[5::3])
+print(c[:5:2])
+print(c[10:14:2])
+print(c[-10:-3:-3])
+print(c[5:12:5])
+print(c[3:-3:3])
+print(c[-5:-3:20])
+
+
+
